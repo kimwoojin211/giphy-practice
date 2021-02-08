ec047f31cf570a1f20a82ef93df19a75c68a5f26
Scott O'Neil <scottieoneil@gmail.com>
2021-02-08 11:59:27 -0800

    add initial file skeleton
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>
    Co-authored-by: Woo Jin Kim <kimwoojin211@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'apiprac',
+      title: 'giphyprac',
       template: './src/index.html',
       inject: 'body'
     }),
